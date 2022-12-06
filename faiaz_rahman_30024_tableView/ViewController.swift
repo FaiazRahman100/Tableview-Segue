1ac0af1bfc3730ace08fbf27b1ad0ec4c40a4d96
faiaz Rahman <faiaz.rahman@bjitacademy.com>
2022-12-06 07:55:25 +0600

    Initial Commit

---
+++ faiaz_rahman_30024_tableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  faiaz_rahman_30024_tableView
+//
+//  Created by bjit on 6/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
